5a6cf43671781ae841afa635a8268614b10eb753
Lucas Yang <yangchenshin77@gmail.com>
2025-03-09 02:07:43 +0800

    Install tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config'
 import vercel from '@astrojs/vercel'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'http://localhost:4321',
   adapter: vercel(),
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
