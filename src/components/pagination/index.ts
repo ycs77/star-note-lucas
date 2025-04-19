440b14257a2beb4d9c721a2425ef60f1b243e725
Lucas Yang <yangchenshin77@gmail.com>
2025-04-20 01:46:55 +0800

    Add prev/next pagination buttons for post page

---
+++ src/components/pagination/index.ts
@@ -0,0 +1,4 @@
+import Pagination from './Pagination.astro'
+import PaginationPrevNext from './PaginationPrevNext.astro'
+
+export { Pagination, PaginationPrevNext }
--- src/components/postlist/index.ts
+++ src/components/postlist/index.ts
@@ -1,5 +1,4 @@
 import Postlist from './Postlist.astro'
 import PostlistNav from './PostlistNav.astro'
-import Pagination from './Pagination.astro'
 
-export { Postlist, PostlistNav, Pagination }
+export { Postlist, PostlistNav }
