b343b3f6531a301716498c18c85912071c779063
Lucas Yang <yangchenshin77@gmail.com>
2025-05-21 01:58:09 +0800

    Improve og images filename

--- src/pages/og/[filename].jpg.ts
+++ src/pages/og/[filename].jpg.ts
@@ -3,23 +3,23 @@ import type { AstroComponentFactory } from 'astro/runtime/server/index.js'
 import type { CollectionEntry } from 'astro:content'
 import { getOGImage } from '@/og'
 import { getPostCollection } from '@/post'
-import { parsePostSlug } from '@/utils/slug'
 
 export async function getStaticPaths() {
   const posts = await getPostCollection()
   const templates = import.meta.glob('../../content/og/*.astro')
-  return await Promise.all(
+  const paths = await Promise.all(
     posts.map(async post => {
       const templatePath = `../../content/og/${post.id}.astro`
       const template: AstroComponentFactory | undefined = templates[templatePath]
         ? await templates[templatePath]().then((m: any) => m.default || m)
         : undefined
       return {
-        params: { slug: parsePostSlug(post).slug },
+        params: { filename: post.id },
         props: { post, template },
       }
     })
   )
+  return paths.filter(({ props }) => props.template)
 }
 
 export const GET: APIRoute = async ({ props }) => {
