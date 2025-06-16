fee85bf24b94885363cfae9eb5b832c57eb23161
Lucas Yang <yangchenshin77@gmail.com>
2025-06-17 00:57:35 +0800

    Update domain

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 星星的筆記．Lucas
 
-使用 [Astro](https://astro.build/) 構建的部落格 [星星的筆記．Lucas](https://star-note-lucas.vercel.app/)。
+使用 [Astro](https://astro.build/) 構建的部落格 [星星的筆記．Lucas](https://star-note-lucas.me/)。
 
 ## 開發
 
--- src/content/friendLinks.ts
+++ src/content/friendLinks.ts
@@ -4,7 +4,7 @@ export default <FriendLink[]>[
   // {
   //   name: '星星的筆記．Lucas',
   //   description: '即使是一顆小星星 也會閃耀著光芒✨',
-  //   link: 'http://star-note-lucas.vercel.app/',
-  //   avatar: 'http://star-note-lucas.vercel.app/logo.png',
+  //   link: 'http://star-note-lucas.me/',
+  //   avatar: 'http://star-note-lucas.me/logo.png',
   // },
 ]
