2e663635ea70a24521fd02d1692f66b23af74669
Lucas Yang <yangchenshin77@gmail.com>
2025-09-25 23:10:20 +0800

    Improve post page container

--- src/components/article/index.ts
+++ src/components/article/index.ts
@@ -1,6 +1,7 @@
+import ArticleContainer from './ArticleContainer.astro'
 import ArticleContent from './ArticleContent.astro'
 import ArticleLicense from './ArticleLicense.astro'
 import ArticleMediumZoom from './ArticleMediumZoom.astro'
 import ArticleTitle from './ArticleTitle.astro'
 
-export { ArticleContent, ArticleLicense, ArticleMediumZoom, ArticleTitle }
+export { ArticleContainer, ArticleContent, ArticleLicense, ArticleMediumZoom, ArticleTitle }
